19e1425060cc606ad43daf81b135b9aac08a8554
Mayank7371 <neprone.dev@gmail.com>
2025-09-17 18:37:49 +0530

    add server files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import { useState, useEffect } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [products, setProducts] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch("http://localhost:4600/api/products");
+      const products = await res.json();
+      setProducts(products);
+    }
+    fetchData();
+  }, []);
 
   return (
     <>
       <div className="bg-slate-900 h-20">
         <div className="text-3xl text-amber-300 grid place-content-center ">
-          Hi there
+          <div>
+            <h1></h1>
+            <ul>
+              {products.map((p) => {
+                <li key={p.id}>
+                  <strong>{p.name}</strong> -${p.price}
+                </li>;
+              })}
+            </ul>
+          </div>
         </div>
       </div>
     </>
